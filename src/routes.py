65952aca45640e0eeecc70ef6409a95fede1af7b
UmiKami <ggoku02@gmail.com>
2023-03-02 00:01:46 -0500

    added like count parsing

--- src/routes.py
+++ src/routes.py
@@ -89,10 +89,13 @@ def get_video_details(id):
 
     if "Item" in videoDetails:
         comments = videoDetails["Item"]["comments"]["SS"]
+        rating = videoDetails["Item"]["ratin"]["S"]
 
         # turns array of strings to array of json objects
         for index, comment in enumerate(comments):
             comments[index] = literal_eval(json.loads(json.dumps(comment)))
+
+        rating = literal_eval(json.loads(json.dumps(rating)))
 
         return jsonify(videoDetails["Item"]), 200
     
